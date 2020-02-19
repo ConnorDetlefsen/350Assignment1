cb0da009789c3367da90934d79992a4b6b97d5ba
Connor Detlefsen <connordetlefsen@gmail.com>
2020-02-19 22:45:20 +0000

    added probability and variance

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 350Assignment1
 DNA program
 GeeksforGeeks.org to learn about file input / output
+http://www.cplusplus.com/ for learning about syntax and cmath library
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream> //preprocessor directive
 #include <fstream> //file streams
 #include <cctype> //used for to lower
+#include <cmath> //used for sqrt
+
 
 using namespace std;  //namespace=collection of classes
 
@@ -12,11 +14,14 @@ int main(int argc, char** argv){
 
   ifstream fin; //creation of ifstream class object
   ofstream fout;  //creates ofstream object
-  
+
   string line = "";
   int A = 0, G = 0, T = 0, C = 0;
-  int AA = 0, AG = 0, AC = 0, AT = 0, GG = 0, GA = 0, GT = 0, GC = 0, TT = 0, TA = 0, TG = 0, TC = 0, CC = 0, CA = 0, CG = 0, CT = 0;
-  int LineCount = -1; //figure this out
+  float AA = 0, AG = 0, AC = 0, AT = 0, GG = 0, GA = 0, GT = 0, GC = 0, TT = 0, TA = 0, TG = 0, TC = 0, CC = 0, CA = 0, CG = 0, CT = 0;
+  float LineCount = -1; //figure this out
+  float bigramCount = 0;
+  int sum = 0;
+
   fin.open(fileName);
     while (fin) {     //reads in input from text file given from user
       getline(fin, line);
@@ -35,7 +40,10 @@ int main(int argc, char** argv){
         else if(c == 'C')
           C++;
       }
+      sum = A+G+T+C;
+
       for(int i = 0; i < line.length(); i = i + 2){ //finds the pairs of DNA nucleotides
+        bigramCount++;
         char c = line[i];
         char d = line[i+1];
         string putTogether;
@@ -76,9 +84,26 @@ int main(int argc, char** argv){
           TC++;
       }
   }
-  //fin.close();
+  fin.close();
 
-  int sum = A+G+T+C;
+  float mean = float(sum) / float(LineCount);
+  float AProb = float(A) / float(sum);
+  float GProb = float(G) / float(sum);
+  float CProb = float(C) / float(sum);
+  float TProb = float(T) / float(sum);
+
+
+  //variance
+  float variance = 0;
+  fin.open(fileName);
+    for(int i = 0; i < LineCount; i++){     //reads in input from text file given from user
+      getline(fin, line);
+      variance = variance+ pow((line.length() - mean),2);  //figure this out
+    }
+   variance = variance / LineCount;
+
+
+
 
   string info = "Connor Detlefsen \n2312185 \nCPSC 350-02 \nAssignment 1";      //this bit adds my info to the top of the file
 //  ofstream fout;  //creates ofstream object
@@ -87,16 +112,18 @@ int main(int argc, char** argv){
   fout.close();
 
   fout.open("connordetlefsen.out", ios::app); // allows us to append to contents of file instead of erasing it
-  fout << "A count: " << A << endl;  // write the count to the "connordetlefsen.out" file
-  fout << "G count: " << G << endl;
-  fout << "T count: " << T << endl;
-  fout << "C count: " << C << endl;
-  fout << "Sum: " << sum << endl;
-  fout << "Number of Lines: " << LineCount << endl;
-  fout << "AA: " << AA << "\nAG: " << AG << "\nAC: " << AC << "\nAT: " << AT << endl;
-  fout << "GG: " << GG << "\nGA: " << GA << "\nGC: " << GC << "\nGT: " << GT << endl;
-  fout << "CC: " << CC << "\nCG: " << CG << "\nCA: " << CA << "\nCT: " << CT << endl;
-  fout << "TT: " << TT << "\nTG: " << TG << "\nTC: " << TC << "\nTA: " << TA << endl;
+  fout << "A probability: " << AProb << endl;  // write the count to the "connordetlefsen.out" file
+  fout << "G probability " << GProb << endl;
+  fout << "T probability: " << TProb << endl;
+  fout << "C probability: " << CProb  << endl;
+  fout << "Sum of the length of the DNA strings: " << sum << endl;
+  //fout << "Number of Lines: " << LineCount << endl;
+  fout << "Mean of the length of the DNA strings: " << mean << endl;
+  fout << "Variance of the length of the DNA strings: " << variance << endl;
+  fout << "AA: " << AA/bigramCount << "\nAG: " << AG/bigramCount << "\nAC: " << AC/bigramCount << "\nAT: " << AT/bigramCount << endl;
+  fout << "GG: " << GG/bigramCount << "\nGA: " << GA/bigramCount << "\nGC: " << GC/bigramCount << "\nGT: " << GT/bigramCount << endl;
+  fout << "CC: " << CC/bigramCount << "\nCG: " << CG/bigramCount << "\nCA: " << CA/bigramCount << "\nCT: " << CT/bigramCount << endl;
+  fout << "TT: " << TT/bigramCount << "\nTG: " << TG/bigramCount << "\nTC: " << TC/bigramCount << "\nTA: " << TA/bigramCount << endl;
 
   fin.close();
   fout.close();
